3d437f79b4892b0519d7df344ddb2f1dffe13438
LesLie-Wmy <133620294+LesLie-Wmy@users.noreply.github.com>
2026-03-23 21:36:20 +0800

    Change default IP from 192.168.30.1 to 192.168.1.1

--- 25.12/diy-part2.sh
+++ 25.12/diy-part2.sh
@@ -81,7 +81,7 @@ if [ -f "$RUST_FILE" ]; then
 	cd $PKG_PATH && echo "rust has been fixed!"
 fi
 
-# 修改默认 IP (192.168.30.1)
-sed -i 's/192.168.6.1/192.168.30.1/g' package/base-files/files/bin/config_generate
+# 修改默认 IP (192.168.1.1)
+sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 echo "✅ SSH2 配置完成。"
